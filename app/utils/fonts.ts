5021f8865c9b799a84432282b5baa858f46777bc
Omar Ahmed <o.ahmed@plementus.com>
2025-01-18 14:45:41 +0200

    lol

--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -1,5 +1,7 @@
 import { Cairo } from "next/font/google";
+
 export const cairo = Cairo({
+  subsets: ["arabic"], // Specify the subsets you need (e.g., 'arabic', 'latin', 'latin-ext')
   display: "swap",
-  weight: ["1000", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"], // Ensure weights are valid
 });
